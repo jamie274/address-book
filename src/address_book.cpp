b882dc1be21956db14ad5135d3e45aa105f113d9
jamie274 <jamie-lee.gordon@city.ac.uk>
2023-01-20 17:17:30 +0000

    Add project files.

---
+++ src/address_book.cpp
@@ -0,0 +1,47 @@
+
+
+#include "include/address_book.h"
+
+#include <stdexcept>
+
+void AddressBook::add(AddressBook::Entry person)
+{
+	// Your code here
+	//
+
+	throw std::runtime_error("Not implemented");
+}
+
+void AddressBook::remove(AddressBook::Entry person)
+{
+	// Your code here
+	//
+
+	throw std::runtime_error("Not implemented");
+}
+
+std::vector<AddressBook::Entry> AddressBook::sortedByFirstName()
+{
+	// Your code here
+	//
+
+	throw std::runtime_error("Not implemented");
+}
+
+std::vector<AddressBook::Entry> AddressBook::sortedByLastName()
+{
+	// Your code here
+	//
+
+
+	throw std::runtime_error("Not implemented");
+}
+
+std::vector<AddressBook::Entry> AddressBook::find(const std::string& name)
+{
+	// Your code here
+	//
+
+
+	throw std::runtime_error("Not implemented");
+}
